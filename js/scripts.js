d9b609472173f475d84e4f7c3b17c4cbf8d2c92f
Jackie Fletcher & Ron Park <student@epicodus.com>
2015-02-12 10:44:02 -0800

    toString hack

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+var binary = function(number) {
+
+var number = parseInt(number);
+var binary = Number(number).toString(2);
+return binary;
+// var ranger = [1..10];
+// var binary = ["0"];
+//
+// for(var index = 0; number <= (ranger[index]); index++) {
+//   if binary[binary.length-1] === 0 {
+//     binary = binary.pop
+//     binary.push("1")
+//   }
+// };
+
+}
+
+// $(document).ready(function() {
+//   $('form#scrabble').submit(function(event) {
+//     var word = $("input#word").val();
+//     var result = scrabble(word);
+//
+//     $(".points").text(result);
+//
+//     $("#result").show();
+//     event.preventDefault();
+//   });
+//
+// });
---
+++ spec/specs.js
@@ -0,0 +1,6 @@
+describe("binary", function() {
+  it("will return the binary code of a number under 10", function() {
+    expect(binary("3")).to.equal("11");
+  });
+
+});
